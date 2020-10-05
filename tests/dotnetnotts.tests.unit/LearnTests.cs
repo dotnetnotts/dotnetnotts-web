91362d976ba20d917a69d17e2da3b8b0c9f0dd1b
Luke <luke.o.billington@gmail.com>
2020-10-05 07:55:41 +0100

    Feature: unit / component tests for frontend (#56)
    * Created testing project with bUnit, added basic Index test
    
    * Moved to xUnit to reduce dependencies, and added additional tests
    
    * Updated README.md to reflect test changes
    
    * Merge with origin, updated test
    
    * Force move 'Contactus.razor' -> 'ContactUs.razor'
    
    * Updated tests to reflect new changes

---
+++ tests/dotnetnotts.tests.unit/LearnTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Bunit;
+using dotnetnotts.Pages;
+using Xunit;
+
+namespace dotnetnotts.tests.unit
+{
+    public class LearnTests : IDisposable
+    {
+        private readonly TestContext _context;
+        private readonly IRenderedComponent<Learn> _learn;
+
+        public LearnTests()
+        {
+            _context = new TestContext();
+            _learn = _context.RenderComponent<Learn>();
+        }
+
+        [Fact]
+        public void LearnTitleIsDisplayed()
+        {
+            Assert.Contains("<h1 tabindex=\"1\">Learn</h1>", _learn.Markup);
+        }
+
+        [Fact]
+        public void HeadersForLearningSectionsAreDisplayed()
+        {
+            var learnCards = _learn.FindAll(".card");
+            var cardHeaders = learnCards.Children(".card-header").ToList();
+
+            Assert.Single(cardHeaders, header => header.ToMarkup().Contains("Learn .NET"));
+            Assert.Single(cardHeaders, header => header.ToMarkup().Contains("Learn Azure"));
+        }
+
+        [Fact]
+        public void AllLinksToLearningResourcesAreProvided()
+        {
+            var learnCards = _learn.FindAll(".card");
+            var links = learnCards
+                .Children(".list-group-item a")
+                .Select(a => a as IHtmlAnchorElement);
+
+            Assert.All(links, link => Uri.IsWellFormedUriString(link?.Href,
+                UriKind.RelativeOrAbsolute));
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
