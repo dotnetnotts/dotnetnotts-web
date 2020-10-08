3be0b81c82c0b4c8e8f01f76cb905d26884186b2
Mark Oliver <71320282+mosoftwareenterprises@users.noreply.github.com>
2020-10-08 08:36:39 +0100

    Add Speaker Information #issue13 (#60)
    * Hacktober Feature Request: Add Content To Speaker Info Page
    Fixes #13
    
    * Add tests relevant to the page.
    
    * Ensure Unit tests work

--- tests/dotnetnotts.tests.unit/SpeakerInfoTests.cs
+++ tests/dotnetnotts.tests.unit/SpeakerInfoTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using AngleSharp.Html.Dom;
 using Bunit;
 using dotnetnotts.Pages;
 using Xunit;
@@ -8,20 +10,83 @@ namespace dotnetnotts.tests.unit
     public class SpeakerInfoTests : IDisposable
     {
         private readonly TestContext _context;
-        private readonly IRenderedComponent<SpeakerInfo> _speakerInfo;
-        
+        private readonly IRenderedComponent<SpeakerInfo> _systemUnderTest;
+
         public SpeakerInfoTests()
         {
             _context = new TestContext();
-            _speakerInfo = _context.RenderComponent<SpeakerInfo>();
+            _systemUnderTest = _context.RenderComponent<SpeakerInfo>();
         }
 
         [Fact]
-        public void SpeakerInfoTitleIsDisplayed()
+        public void CodeOfConductSectionExists()
         {
-            Assert.Contains("<h3 tabindex=\"1\">Speaker Info</h3>", _speakerInfo.Markup);
+            var codeOfConduct = _systemUnderTest.Find("#code-of-conduct");
+            Assert.NotNull(codeOfConduct);
+        }
+
+        [Fact]
+        public void CodeOfConductLinkIsCorrect()
+        {
+            var codeOfConductLink = _systemUnderTest.Find("#code-of-conduct-link");
+            Assert.NotNull(codeOfConductLink);
+        }
+
+        [Fact]
+        public void VirtualMeetupsSectionExists()
+        {
+            var section = _systemUnderTest.Find("#virtual-meetups");
+            Assert.NotNull(section);
+        }
+
+        [Fact]
+        public void ScheduleSectionExists()
+        {
+            var section = _systemUnderTest.Find("#schedule");
+            Assert.NotNull(section);
+        }
+               
+        [Fact]
+        public void SocialMediaSectionExists()
+        {
+            var section = _systemUnderTest.Find("#social-media");
+            Assert.NotNull(section);
+        }        
+        
+        [Fact]
+        public void TwitterLinkForDotNetNottsIsProvided()
+        {
+            AssertLinkExists("https://twitter.com/dotnetnotts");
         }
         
+        [Fact]
+        public void TwitterLinkForJessIsProvided()
+        {
+            AssertLinkExists("https://twitter.com/dotnetnotts");
+        }
+        
+        [Fact]
+        public void EmailLinkForPeteIsProvided()
+        {
+            AssertLinkExists("mailto:pete@pjgcreations.co.uk");
+        }
+
+        [Fact]
+        public void EmailLinkForJessIsProvided()
+        {
+            AssertLinkExists("mailto:me@jesswhite.co.uk");
+        }
+        
+        private void AssertLinkExists(string link)
+        {
+            var linkExists = _systemUnderTest
+                .FindAll("a")
+                .Select(a => a as IHtmlAnchorElement)
+                .Any(a => a?.Href == link);
+
+            Assert.True(linkExists);
+        }
+
         public void Dispose()
         {
             _context.Dispose();
