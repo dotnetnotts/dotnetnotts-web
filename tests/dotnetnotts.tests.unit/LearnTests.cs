49d4d09eca09da3c1c558f0bb4179dc150beebb6
Jessica <me@jesswhite.co.uk>
2021-01-09 18:50:50 +0000

    Update Learn Page and tests

--- tests/dotnetnotts.tests.unit/LearnTests.cs
+++ tests/dotnetnotts.tests.unit/LearnTests.cs
@@ -20,19 +20,9 @@ namespace dotnetnotts.tests.unit
         }
 
         [Fact]
-        public void LearnTitleIsDisplayed()
+        public void ResourcesTitleIsDisplayed()
         {
-            Assert.Contains("<h1 tabindex=\"1\">Learn</h1>", _learn.Markup);
-        }
-
-        [Fact]
-        public void HeadersForLearningSectionsAreDisplayed()
-        {
-            var learnCards = _learn.FindAll(".card");
-            var cardHeaders = learnCards.Children(".card-header").ToList();
-
-            Assert.Single(cardHeaders, header => header.ToMarkup().Contains("Learn .NET"));
-            Assert.Single(cardHeaders, header => header.ToMarkup().Contains("Learn Azure"));
+            Assert.Contains("<h1 tabindex=\"0\">Resources Page</h1>", _learn.Markup);
         }
 
         [Fact]
@@ -45,6 +35,70 @@ namespace dotnetnotts.tests.unit
 
             Assert.All(links, link => Uri.IsWellFormedUriString(link?.Href,
                 UriKind.RelativeOrAbsolute));
+        }
+
+        [Fact]
+        public void LearnDotNetTitleIsDisplayed()
+        {
+            Assert.Contains("<h2 tabindex=\"0\">Learn .NET</h2>", _learn.Markup);
+        }
+
+        [Fact]
+        public void MicrosoftLearnLinkIsProvided()
+        {
+            AssertLinkExists("https://dotnet.microsoft.com/learn");
+        }
+
+        [Fact]
+        public void CSharpFirstStepsLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/learn/paths/csharp-first-steps/?WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        [Fact]
+        public void DotNetDocsLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/dotnet/?WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        [Fact]
+        public void AspDocsLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/aspnet/core/?view=aspnetcore-3.1&WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        [Fact]
+        public void CSharpDataLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/learn/paths/csharp-data/?WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        [Fact]
+        public void LearnAzureTitleIsDisplayed()
+        {
+            Assert.Contains("<h2 tabindex=\"5\">Learn Azure</h2>", _learn.Markup);
+        }
+
+        [Fact]
+        public void AzureDocumentationLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/azure/?product=featured#languages-and-tools&WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        [Fact]
+        public void QuickstartLinkIsProvided()
+        {
+            AssertLinkExists("https://docs.microsoft.com/en-gb/azure/azure-functions/functions-create-first-azure-function-azure-cli?pivots=programming-language-csharp&tabs=bash%2Cbrowser&WT.mc_id=cloudskills-stream-ajack");
+        }
+
+        private void AssertLinkExists(string link)
+        {
+            var linkExists = _learn
+                .FindAll("a")
+                .Select(a => a as IHtmlAnchorElement)
+                .Any(a => a?.Href == link);
+
+            Assert.True(linkExists);
         }
 
         public void Dispose()
