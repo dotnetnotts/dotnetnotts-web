5cdc35ca04d4a9e03c5633ee03bb65ee3a67f341
Jessica <me@jesswhite.co.uk>
2022-01-29 18:37:30 +0000

    Fix tests

--- tests/dotnetnotts.tests.unit/IndexTests.cs
+++ tests/dotnetnotts.tests.unit/IndexTests.cs
@@ -49,12 +49,6 @@ namespace dotnetnotts.tests.unit
         {
             AssertLinkExists("https://www.youtube.com/channel/UC6N65W35hZMcVXeqq3Vi5Iw");
         }
-        
-        [Fact]
-        public void UpcomingEventsTitleIsDisplayed()
-        {
-            Assert.Contains("<h2 tabindex=\"0\">Upcoming Events</h2>", _index.Markup);
-        }
 
         [Fact]
         public void PastEventsTitleIsDisplayed()
